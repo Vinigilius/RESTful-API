c7e053aa108789c2b117ddd67833b0f044ead225
Łukasz Pochrzęst <lukasz.pochrzest@gmail.com>
2018-04-22 23:03:12 +0200

    Secon commit with basic features.

--- RESTful-API/Program.cs
+++ RESTful-API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace RESTful_API
+namespace RESTfulAPI
 {
     public class Program
     {
--- RESTful-API/Startup.cs
+++ RESTful-API/Startup.cs
@@ -5,30 +5,50 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using RESTfulAPI.Entities;
+using RESTfulAPI.Services;
 
-namespace RESTful_API
+namespace RESTfulAPI
 {
     public class Startup
     {
+
+        public static IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            var connectionString = Configuration["conncetionStrings:libraryDBConncectionString"];
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, LibraryContext libraryContext)
         {
-            if (env.IsDevelopment())
-            {
+            
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseMvc();
+
         }
     }
 }
